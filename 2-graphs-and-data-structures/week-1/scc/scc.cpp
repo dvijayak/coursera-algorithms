fb03c234c991583c4442198f2b558613a139a4cf
Daniel Vijayakumar <daniel.vkumar@gmail.com>
2020-11-28 18:16:28 -0500

    Finished the Strongly Connected Components assignment. It was definitely challenging.

--- 2-graphs-and-data-structures/week-1/scc/scc.cpp
+++ 2-graphs-and-data-structures/week-1/scc/scc.cpp
@@ -1,16 +1,15 @@
 /**
- * Your task is to code up and run the randomized contraction algorithm for the min cut problem and use it on the given graph to compute the min cut.
- * (HINT: Note that you'll have to figure out an implementation of edge contractions. Initially, you might want to do this naively, creating a new graph from the old every time there's an edge contraction. But you should also think about more efficient implementations.)
- * (WARNING: As per the video lectures, please make sure to run the algorithm many times with different random seeds, and remember the smallest cut that you ever find.)
+ * Your task is to code up the algorithm from the video lectures for computing strongly connected components (SCCs), and to run this algorithm on the given graph.
+ * You should output the sizes of the 5 largest SCCs in the given graph, in decreasing order of sizes, separated by commas (avoid any spaces).
+ * So if your algorithm computes the sizes of the five largest SCCs to be 500, 400, 300, 200 and 100, then your answer should be "500,400,300,200,100" (without the quotes).
+ * If your algorithm finds less than 5 SCCs, then write 0 for the remaining terms. Thus, if your algorithm computes only 3 SCCs whose sizes are 400, 300, and 100, then your answer should be "400,300,100,0,0" (without the quotes). (Note also that your answer should not have any spaces in it.)
  */
 
 #include <iostream>
 #include <fstream>
 #include <dirent.h> // UNIX only; for Windows, need to get it elsewhere, see https://stackoverflow.com/a/612176/3477043
 #include <string>
-#include <regex>
 
-#include <limits>
 #include <cassert>
 #include <memory>
 #include <vector>
@@ -18,7 +17,6 @@
 #include <map>
 #include <unordered_map>
 #include <unordered_set>
-#include <sstream>
 #include <algorithm>
 
 using namespace std;
@@ -60,16 +58,6 @@ ostream &operator<<(ostream &os, array<T, N> const &a)
 
    return os;
 }
-
-// Courtesy of https://stackoverflow.com/a/9437426
-std::vector<std::string> split(const string& input, const string& regex) {
-    // passing -1 as the submatch index parameter performs splitting
-    std::regex re(regex);
-    std::sregex_token_iterator
-        first{input.begin(), input.end(), re, -1},
-        last;
-    return {first, last};
-}
 #pragma endregion
 
 class DirectedGraph
@@ -90,14 +78,12 @@ class DirectedGraph
    };
 
    unordered_map<int, unique_ptr<Node>> nodes;
-   unordered_map<Node*, int> node2key;
 
    // Creates a new node in the graph with a guaranteed unique id/key (uniqueness is only local to the graph)
    Node* MakeNode(int const value, int const key)
    {
       auto p = new Node(key, value);
       nodes.insert(make_pair(key, unique_ptr<Node>(p)));
-      node2key.insert(make_pair(p, key));
       return p;
    }
 
@@ -113,40 +99,64 @@ class DirectedGraph
       to->in_neighbors.insert(from);
    }
 
-   void DFS(bool is_first_pass, Node* node, unordered_set<Node*>& visited, vector<Node*> *finishing_order, vector<int> *scc)
+   void DFS(bool is_first_pass, Node* node, vector<bool>& visited, vector<Node*> *finishing_order, vector<int> *scc)
    {
       if (node == nullptr)
          return;
 
-      // Mark as explored
-      visited.insert(node);
+      // Init
+      vector<bool> finished(N(), false); // tracks whether the node has completed recording its finishing order (used only during first pass)
+      vector<Node*> stack;
+      stack.push_back(node);
 
-      cout << "\t" << node->key() << endl;
-
-      // Associate with SCC, only on second pass
-      if (!is_first_pass && scc != nullptr)
+      // Begin
+      while (!stack.empty())
       {
-         scc->push_back(node->key());
-      }
-      
-      // First pass DFS is done on the graph with reverse edges
-      // This is equivalent to exploring the node's incoming edges
-      // Second pass DFS is performed normally i.e. following outgoing edges of the node
-      unordered_set<Node*> *p_neighbors = is_first_pass ? &(node->in_neighbors) : &(node->out_neighbors);
-      
-      // Visit neighors
-      for (Node* neighbor : *p_neighbors)
-      {
-         if (visited.find(neighbor) != visited.end()) // already seen
-            continue;
+         // Peek and process
+         Node* node = stack[stack.size() - 1];
+         int key = node->key();
 
-         DFS(is_first_pass, neighbor, visited, finishing_order, scc);
-      }
+         // Check neighbors if unexplored
+         if (!visited[key])
+         {
+            // Mark as explored
+            visited[key] = true;
 
-      // "Record" finishing time, only on first pass
-      if (is_first_pass && finishing_order != nullptr)
-      {
-         finishing_order->push_back(node);
+            // Associate with SCC, only on second pass
+            if (!is_first_pass && scc != nullptr)
+            {
+               scc->push_back(node->key());
+            }
+
+            // First pass DFS is done on the graph with reverse edges
+            // This is equivalent to exploring the node's incoming edges
+            // Second pass DFS is performed normally i.e. following outgoing edges of the node
+            unordered_set<Node*> *neighbors = is_first_pass ? &(node->in_neighbors) : &(node->out_neighbors);
+            
+            // Visit neighors
+            for (Node* neighbor : *neighbors)
+            {
+               if (visited[neighbor->key()]) // already seen
+                  continue;
+
+               stack.push_back(neighbor);
+            }
+         }
+         // Otherwise it means that all neighbors have been processed, so we can finish processing this one
+         else
+         {
+            // "Record" finishing time, only on first pass
+            // Note that due to the nature of the DFS stack-based implementation, the already visited node can 
+            // appear more than once on the stack. But we only want to push into `finishing_order` exactly once
+            // in order for the algorithm to be correct. We use the `finished` cache to deduplicate this.
+            if (is_first_pass && finishing_order != nullptr && !finished[key])
+            {
+               finished[key] = true;
+               finishing_order->push_back(node);
+            }
+            
+            stack.pop_back();
+         }
       }
    }
 
@@ -191,44 +201,29 @@ public:
    // Returns list of SCCs, first element of each SCC is the leader node
    vector<vector<int>> GetStronglyConnectedComponents()
    {
-      // 1. Assign arbitrary indices to each node. We will simply copy the nodes to a vector and use this implicit ordering.
-      // vector<Node*> nodes_arbitrary_order;
-      // for (auto const& node : node2key)
-      // {
-      //    nodes_arbitrary_order.push_back(node.first);
-      // }
-
-      // 2. Perform first-pass DFS on the graph with REVERSED edges, computing the "finishing times" label for each node.
+      // 1. Perform first-pass DFS on the graph with REVERSED edges, computing the "finishing times" label for each node.
       vector<Node*> finishing_order;
       {
-         unordered_set<Node*> visited;
-         // for (int i = nodes_arbitrary_order.size(); i > 0; --i)
-         // {
-         //    Node* node = nodes_arbitrary_order[i - 1];
-         //    if (visited.find(node) == visited.end()) // not yet explored
-         //    {
-         //       DFS(true, node, visited, &finishing_order, nullptr);
-         //    }
-         // }
-         for (auto node : node2key)
+         vector<bool> visited(N(), false);
+         for (auto & node : nodes)
          {
-            if (visited.find(node.first) == visited.end()) // not yet explored
+            if (!visited[node.second.get()->key()]) // not yet explored
             {
-               cout << node.first->key() << endl;
-               DFS(true, node.first, visited, &finishing_order, nullptr);
+               DFS(true, node.second.get(), visited, &finishing_order, nullptr);
             }
          }
       }
 
-      // 3. Perform second-pass DFS on the graph with original edges, this time grouping nodes into their corresponding SCC.
+      // 2. Perform second-pass DFS on the graph with original edges, this time grouping nodes into their corresponding SCC.
       vector<vector<int>> sccs;
       {
-         unordered_set<Node*> visited;
+         // set<Node*> visited;
+         vector<bool> visited(N(), false);
          for (int i = finishing_order.size(); i > 0; --i)
          {
             vector<int> scc;
             Node* node = finishing_order[i - 1];
-            if (visited.find(node) == visited.end()) // not yet explored
+            if (!visited[node->key()]) // not yet explored
             {
                DFS(false, node, visited, nullptr, &scc);
             }
@@ -236,8 +231,6 @@ public:
                sccs.push_back(scc);
          }
       }
-
-      // cout << sccs << endl;
 
       return move(sccs);
    }
@@ -252,8 +245,6 @@ array<int, 5> GetTop5SCCsSizesDescending(vector<pair<int, int>> const& edge_data
    vector<int> scc_sizes;
    transform(sccs.begin(), sccs.end(), back_inserter(scc_sizes), [&](vector<int> const& scc) { return scc.size(); });
    sort(scc_sizes.begin(), scc_sizes.end(), [](int const a, int const b) { return a > b; });
-
-   // cout << scc_sizes << endl;
 
    // Sanity check: The sum of all sizes should be the total number of nodes in the graph;
    {
